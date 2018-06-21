9bcf875ac7e7537be3204873f7378da5d8584614
Duncan <marjoribanks.d@gmail.com>
2018-06-21 15:40:38 +0100

    properties on page are converted to new objects in app.js

--- lab_events_start/js/app.js
+++ lab_events_start/js/app.js
@@ -1,3 +1,24 @@
 document.addEventListener('DOMContentLoaded', () => {
   console.log('JavaScript loaded');
+
+const form = document.querySelector('#new-item-form');
+
+const handleSubmit = function() {
+  this.preventDefault();
+
+  const title = this.querySelector('#title');
+  const author = this.querySelector('#author');
+  const category = this.querySelector('#category');
+
+
+
+}
+
+
+
+
+
+
+
+
 })
