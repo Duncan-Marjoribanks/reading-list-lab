3a7a6992fc4b1c91c79a305a9a319543d401fbf7
Duncan <marjoribanks.d@gmail.com>
2018-06-21 16:09:18 +0100

    can create new records on form submission

--- lab_events_start/js/app.js
+++ lab_events_start/js/app.js
@@ -3,18 +3,26 @@ document.addEventListener('DOMContentLoaded', () => {
 
 const form = document.querySelector('#new-item-form');
 
-const handleSubmit = function() {
-  this.preventDefault();
+const handleSubmit = function(event) {
+  event.preventDefault();
 
   const title = this.querySelector('#title');
   const author = this.querySelector('#author');
   const category = this.querySelector('#category');
 
+  const titleText = title.value;
+  const authorText = author.value;
+  const categorySelection = category.value;
 
+  const newRecord = document.createElement('p');
+  const recordString = `${titleText} by ${authorText} is a ${categorySelection}.`;
+  newRecord.textContent = recordString;
 
+  const readingList = document.querySelector('#reading-list');
+  readingList.appendChild(newRecord);
 }
 
-
+form.addEventListener('submit', handleSubmit);
 
 
 
